22e61dc4157b58bf5109df0302a72a32f2326ba4
Масальских Иван Сергеевич <imasalskikh@stream.ru>
2019-12-30 20:02:24 +0500

    Project and pod init

---
+++ ISDKTest/ISDKTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ISDKTest
+//
+//  Created by Иван Масальских on 30/12/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
