07da279fef7bbf631dc7f564573a9aa517b3d75d
Масальских Иван Сергеевич <imasalskikh@stream.ru>
2020-01-12 15:04:05 +0500

    ItemBRepository add logic

--- ISDKTest/ISDKTest/Classes/Data/Repositories/ItemB/ItemBRepository.swift
+++ ISDKTest/ISDKTest/Classes/Data/Repositories/ItemB/ItemBRepository.swift
@@ -11,27 +11,35 @@ import RxSwift
 
 // MARK: - ItemBRepository
 
-final class ItemBRepository {}
-
-extension ItemBRepository: ItemBRepositoryProtocol {
+final class ItemBRepository {
     
-    func getAllItems() -> Observable<[ItemBEntity]> {
-        return Observable.just(Stub.items)
-    }
+    // MARK: Private properties
     
-    func getItem(id: String) -> Observable<ItemBEntity?> {
-        return Observable.just(Stub.items).map { $0.filter { $0.id == id }.first }
+    private let localStorage: ItemBLocalStorageProtocol
+    
+    // MARK: Constructors
+    
+    init(localStorage: ItemBLocalStorageProtocol) {
+        self.localStorage = localStorage
     }
 }
 
-private extension ItemBRepository {
+extension ItemBRepository: ItemBRepositoryProtocol {
     
-    enum Stub {
-        static let items = [
-            ItemBEntity(id: "0", title: "A B One One One One One One One", desc: "One desc One desc One desc One desc One desc One desc", value: 123456789, image: Data(imageName: "Icons/noImage"), labels: ["labels", "labels", "labels", "labels", "labels", "labels"]),
-            ItemBEntity(id: "1", title: "B B Two Two Two Two v Two Two Two Two Two v Two Two Two Two v v Two", desc: "Two desc Two desc Two desc Two desc Two desc Two desc Two desc Two desc v Two desc Two desc", value: 1234567891011121, image: Data(imageName: "Icons/noImage"), labels: ["labels"]),
-            ItemBEntity(id: "2", title: "C B Three Three", desc: "Three desc", value: 123, image: Data(imageName: "Icons/noImage"), labels: ["labels", "labels", "labels", "labels", "labels", "labels", "labels", "labels", "labels"]),
-            ItemBEntity(id: "3", title: "D B Four Four", desc: "Four desc", value: 123, image: Data(imageName: "Icons/noImage"), labels: ["labels1", "labels2", "labels3", "labels4", "labels5"])
-        ]
+    func createItem(title: String, desc: String, value: Int, image: Data, labels: [String]) -> Observable<ItemBEntity?> {
+        return localStorage.createItemB(title: title, desc: desc, value: value, image: image, labels: labels)
+    }
+    
+    func updateItem(_ item: ItemBEntity) -> Observable<Void> {
+        return localStorage.updateItemB(item)
+    }
+    
+    
+    func getAllItems() -> Observable<[ItemBEntity]> {
+        return localStorage.readAllItemB()
+    }
+    
+    func getItem(id: String) -> Observable<ItemBEntity?> {
+        return localStorage.readItemB(id: id)
     }
 }
